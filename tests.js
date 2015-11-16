38baf8139bd3b72bc4f677c0f3e024550c6d02a6
Victor <yumaa.verdin@gmail.com>
2015-11-16 23:08:39 +0300

    Update tests.js
    одинаковые теги могут повторяться

--- tests.js
+++ tests.js
@@ -47,6 +47,28 @@ var tests = {
         }
     },
 
+    test_default_2: {
+        messages: {
+            msg1: {from: 'jack@example.com', to: 'jill@example.org'},
+            msg2: {from: 'noreply@spam.com', to: 'jill@example.org'},
+            msg3: {from: 'boss@work.com', to: 'jack@example.com'}
+        },
+        rules: [
+            {from: '*@work.com', action: 'tag work'},
+            {from: '*@spam.com', action: 'tag spam'},
+            {from: '*@spam.com', action: 'tag spam'},
+            {from: '*@spam.com', action: 'tag spam'},
+            {from: '*@spam.com', action: 'tag spam'},
+            {from: 'jack@example.com', to: 'jill@example.org', action: 'folder jack'},
+            {to: 'jill@example.org', action: 'forward to jill@elsewhere.com'}
+        ],
+        expected: {
+            msg1: ['folder jack', 'forward to jill@elsewhere.com'],
+            msg2: ['tag spam', 'tag spam', 'tag spam', 'tag spam', 'forward to jill@elsewhere.com'],
+            msg3: ['tag work']
+        }
+    },
+
     test_1: {
         messages: {},
         rules: [
